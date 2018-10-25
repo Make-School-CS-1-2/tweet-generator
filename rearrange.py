a16899e85963ae8d34c2787bba35599217c75c8a
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-25 14:04:02 -0700

    Make more modular

--- rearrange.py
+++ rearrange.py
@@ -4,9 +4,9 @@ import random
 
 def rearrange(args):
     random.shuffle(args)
-    print(' '.join(args))
+    return args
 
 
 if __name__ == '__main__':
     arguments = sys.argv[1:];
-    rearrange(arguments)
+    print(' '.join(rearrange(arguments)))
