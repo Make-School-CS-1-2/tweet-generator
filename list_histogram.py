9fe818b28d9fa96284c3c190d22de60d0fc3cf1d
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-30 11:31:09 -0700

    create frequency

--- list_histogram.py
+++ list_histogram.py
@@ -19,12 +19,21 @@ def unique_words(histogram):
     return len(histogram)
 
 
+def frequency(word, histogram):
+    for i in histogram:
+        if word in i[0]:
+            return i[1]
+    return 0
+
+
 if __name__ == '__main__':
     f = open('./don-quixote.txt', mode='r', encoding='utf-8-sig')
     text = " ".join(f.read().split('\n')).lower()
 
-    histogram = histogram(beginner_text)
+    histogram = histogram(text)
+
+    search_word = "don"
 
     print("histogram:", histogram)
     print("number of unique words:", unique_words(histogram))
-    # print("frequency of the word \"don\":", frequency("don", histogram))
+    print("frequency of the word \"%r\":" % search_word, frequency(search_word, histogram))
