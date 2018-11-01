174b4932b89cc4c69e7de84091927829adfd32aa
Severiano Badajoz <sevebadajoz@gmail.com>
2018-11-01 10:46:15 -0700

    Create weighted_random_word and check_randomness()

--- stochastic_sample.py
+++ stochastic_sample.py
@@ -1,9 +1,26 @@
 import dict_histogram
 import random
 
+beginner_text = "one fish two fish red fish blue fish"
+
 
 def get_random_word(histogram):
     return list(histogram.keys())[random.randint(0, len(histogram) - 1)]
+
+
+def weighted_random_word(histogram):
+    word_list = []
+    for key, value in histogram.items():
+        for _ in range(value):
+            word_list.append(key)
+    return word_list[random.randint(0, len(word_list) - 1)]
+
+
+def check_randomness(histogram):
+    words = ""
+    for _ in range(1000):
+        words += weighted_random_word(histogram) + " "
+    return dict_histogram.histogram(words)
 
 
 if __name__ == '__main__':
@@ -13,3 +30,4 @@ if __name__ == '__main__':
 
     histogram = dict_histogram.histogram(text)
 
+    print(weighted_random_word(histogram))
