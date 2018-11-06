021d014705f0312e04e09a674b58ac9042e5aaf2
Severiano Badajoz <sevebadajoz@gmail.com>
2018-11-06 14:19:50 -0800

    Add gunicorn

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    import sys
     f = open('don-quixote.txt', mode='r', encoding='utf-8-sig')
     text = " ".join(f.read().split('\n')).lower()
 
@@ -15,7 +14,7 @@ def hello_world():
     sentence = stochastic_sample.new_weighted_random_word(histogram)
 
     for _ in range(10):
-       sentence += " " + stochastic_sample.new_weighted_random_word(histogram)
+        sentence += " " + stochastic_sample.new_weighted_random_word(histogram)
     return sentence
 
 
