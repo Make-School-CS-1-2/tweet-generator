1248790596adab25d62adf56ca6584b6b91ea08c
Severiano Badajoz <sevebadajoz@gmail.com>
2018-11-13 10:04:41 -0800

    Separate into classes

--- source/listogram.py
+++ source/listogram.py
@@ -19,20 +19,42 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        # DONE: Increase word frequency by count
+        found = False
+        for index in range(len(self)):
+            if self[index][0] == word:
+                self[index] = (word, self[index][1] + count)
+                self.tokens += count
+                found = True
+        if not found:
+            self.append((word, count))
+            self.types += 1
+            self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        # DONE: Retrieve word frequency count
+        for type_tuple in self:
+            if type_tuple[0] == word:
+                return type_tuple[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
-        # TODO: Check if word is in this histogram
+        # DONE: Check if word is in this histogram
+        for type_tuple in self:
+            if type_tuple[0] == word:
+                return True
+        return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
-        # TODO: Implement linear search to find index of entry with target word
+        # Done: Implement linear search to find index of entry with target word
+        for index in range(self):
+            if self[index][0] == target:
+                return index
+        return None
 
 
 def print_histogram(word_list):
