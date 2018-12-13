f33fea8afd018c790003b005f04d46544424d2ce
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-13 15:56:47 -0800

    Remove outdated comment

--- old/stochastic_sample.py
+++ old/stochastic_sample.py
@@ -17,7 +17,7 @@ def weighted_random_word(chain):
 
 
 def new_weighted_random_histogram(chain, chain_sum):
-    count = random.randint(0, chain_sum)  # change the sum to a arg, this is reducing efficiency
+    count = random.randint(0, chain_sum) 
     for key, value in chain.items():
         count -= value.tokens
         if count <= 0:
