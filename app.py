d716ad4b2bf1aa294fe59953c4f27f512b06b150
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-13 15:32:57 -0800

    Move app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from old import stochastic_sample, dict_histogram
 from source.markov_chain import MarkovChain
+import os.path
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    f = open('../text/source.txt', mode='r', encoding='utf-8-sig')
+    f = open(os.getcwd()+'/text/source.txt', mode='r', encoding='utf-8-sig')
     text = " ".join(f.read().split('\n'))
     text = text.split(' ')
     chain = MarkovChain(text)
-
-
     sentence = stochastic_sample.make_sentence(chain)
 
     return ' '.join(sentence)
