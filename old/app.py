6a7ba8306f5a7d2dd392839415e196bd4526e354
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-13 15:07:56 -0800

    Update flask app to use 2nd order markov chain

--- old/app.py
+++ old/app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from old import stochastic_sample, dict_histogram
+from source.markov_chain import MarkovChain
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    f = open('don-quixote.txt', mode='r', encoding='utf-8-sig')
-    text = " ".join(f.read().split('\n')).lower()
+    f = open('../text/source.txt', mode='r', encoding='utf-8-sig')
+    text = " ".join(f.read().split('\n'))
+    text = text.split(' ')
+    chain = MarkovChain(text)
 
-    histogram = dict_histogram.histogram(text)
 
-    sentence = stochastic_sample.new_weighted_random_word(histogram)
+    sentence = stochastic_sample.make_sentence(chain)
 
-    for _ in range(10):
-        sentence += " " + stochastic_sample.new_weighted_random_word(histogram)
-    return sentence
+    return ' '.join(sentence)
